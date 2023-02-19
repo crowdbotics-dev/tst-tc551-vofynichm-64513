32ec1bf58fafd731a185e46ae11b62ea011583bc
Crowdbotics <engineering@crowdbotics.com>
2023-02-19 02:21:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-tc551-vofynichm-64513",
+  url: "https://tst-tc551-vofynichm-64513.botics.co",
+  api: "https://tst-tc551-vofynichm-64513.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
